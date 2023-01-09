64f9d8f58794a982aee5660b5b70dc49b1108a1d
CPSalvador <CPSalvador@F04162.rcgitmanila.com>
2023-01-09 18:31:51 +0800

    commit project

---
+++ SerenityBDD_Bootcamp/src/main/java/com/rgc/test/App.java
@@ -0,0 +1,13 @@
+package com.rgc.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
