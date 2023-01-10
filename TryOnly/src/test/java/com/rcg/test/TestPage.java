d62992081fb30815589c3fdae3b341c1d4c0e396
RVDionisio <RVDionisio@F04302.rcgitmanila.com>
2023-01-10 23:41:26 +0800

    Test

--- TryOnly/src/test/java/com/rcg/test/TestPage.java
+++ TryOnly/src/test/java/com/rcg/test/TestPage.java
@@ -4,8 +4,10 @@ import net.serenitybdd.core.steps.UIInteractions;
 import net.thucydides.core.annotations.Step;
 
 public class TestPage extends UIInteractions {
-@Step("Navigate to Google")
-public void openURL() {
-	openUrl("https://www.google.com/");
+	
+	
+		@Step("Navigate to Google")
+			public void openURL() {
+			openUrl("https://www.google.com/");
 }
 }
--- TryOnly/src/test/java/com/rcg/test/TestRunner.java
+++ TryOnly/src/test/java/com/rcg/test/TestRunner.java
@@ -1,5 +1,30 @@
 package com.rcg.test;
 
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.WebDriver;
+
+import net.serenitybdd.junit5.SerenityJUnit5Extension;
+import net.thucydides.core.annotations.Managed;
+
+
+@ExtendWith(SerenityJUnit5Extension.class)
 public class TestRunner {
 
+	
+	@Managed(driver = "chrome")
+	WebDriver driver;
+	
+	TestPage navigate;
+	
+	
+	@Test
+	public void searchName() {
+		navigate.openURL();
+	}
 }
+
+
+
+
+
